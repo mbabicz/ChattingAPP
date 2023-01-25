987e242528287c89a0c24abb1b3a552df3d08616
kz <michal@babicz.dev>
2023-01-25 22:02:39 +0100

    Initial Commit

---
+++ ChattingAPP/ChattingAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChattingAPPApp.swift
+//  ChattingAPP
+//
+//  Created by kz on 25/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChattingAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
