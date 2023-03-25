2fd581cd6505fce89907d83404f8dac8d6b4c914
Michał Babicz <kiwuzx@gmail.com>
2023-03-25 20:23:33 +0100

    image scaling with MagnificationGesture

--- ChattingAPP/View/ImageViewerView.swift
+++ ChattingAPP/View/ImageViewerView.swift
@@ -9,11 +9,40 @@ import SwiftUI
 
 struct ImageViewerView: View {
     var image: UIImage
+    @State var scale: CGFloat = 1.0
+    @State var lastScaleValue: CGFloat = 1.0
+    @State var isDetectingLongPress = false
+    @State var isDetectingPan = false
+    @GestureState var isDetectingPinch = false
 
     var body: some View {
-        Image(uiImage: image)
+        let magnificationGesture = MagnificationGesture()
+            .updating($isDetectingPinch) { currentState, gestureState, transaction in
+                gestureState = true
+            }
+            .onChanged { scale in
+                self.scale = self.lastScaleValue * scale
+            }
+            .onEnded { scale in
+                let newScale = min(max(scale * self.lastScaleValue, 1), 5.0)
+                self.scale = newScale
+                self.lastScaleValue = newScale
+            }
+
+
+        let pinchToZoom = magnificationGesture.simultaneously(with: TapGesture(count: 2).onEnded({
+            withAnimation {
+                self.scale = 1.0
+                self.lastScaleValue = 1.0
+            }
+        }))
+
+        return Image(uiImage: image)
             .resizable()
             .scaledToFit()
-            .frame(maxWidth: .infinity)
+            .scaleEffect(self.scale)
+            .gesture(pinchToZoom)
+            .animation(.easeInOut(duration: 0.2))
+
     }
 }
