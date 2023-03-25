df768ec389e1d37c66158cf763c398805fda7d8b
Michał Babicz <kiwuzx@gmail.com>
2023-03-25 22:28:34 +0100

    macOS project setup

---
+++ ChatingAPP_macOS/ChatingAPP_macOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatingAPP_macOSApp.swift
+//  ChatingAPP_macOS
+//
+//  Created by kz on 25/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatingAPP_macOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatingAPP_macOS/GlobalChatView.swift
@@ -0,0 +1,20 @@
+//
+//  GlobalChatView.swift
+//  ChatingAPP_macOS
+//
+//  Created by kz on 25/03/2023.
+//
+
+import SwiftUI
+
+struct GlobalChatView: View {
+    var body: some View {
+        Text("GlobalChatView")
+    }
+}
+
+struct GlobalChatView_Previews: PreviewProvider {
+    static var previews: some View {
+        GlobalChatView()
+    }
+}
